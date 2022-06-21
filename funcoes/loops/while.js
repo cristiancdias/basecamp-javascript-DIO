37743819bad7734cd9a18e040ed657f8cf0c5b10
cristiancdias <cristiancdias@gmail.com>
2022-06-21 12:49:06 -0300

    Loops While e doWhile

---
+++ funcoes/loops/while.js
@@ -0,0 +1,18 @@
+function exemploWhile() {
+  let num = 0
+  while (num <= 5) {
+    console.log(num)
+    num++
+  }
+}
+exemploWhile()
+
+function exemploDoWhile() {
+  let num = 6
+  do {
+    console.log(num)
+    num++
+  } while (num <= 5)
+}
+
+exemploDoWhile()
